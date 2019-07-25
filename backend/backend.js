5a61b73f810cd4454952781f8ff4f30d3d821c59
Avner Braverman <avner@binaris.com>
2019-07-25 14:35:30 -0700

    Fix coding style according to template guidelines

--- backend/backend.js
+++ backend/backend.js
@@ -1,11 +1,21 @@
 import { get, update } from '@binaris/shift-db';
 
+/*
+ * Get the current value for the counter.
+ *
+ * @return { number } - counter value
+ */
 /* @expose */
-export async function backendGet() {
+export async function backendGetCounter() {
   return get('counter');
 }
 
+/*
+ * Increment the current counter value by one.
+ *
+ * @return { number } - counter value
+ */
 /* @expose */
-export async function backendIncrement() {
+export async function backendIncrementCounter() {
   return update('counter', c => c + 1, 0);
 }
